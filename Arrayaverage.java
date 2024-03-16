fc7adcf7491d33e5af0a5a7ab1269634bf229260
sumafujiko <sumafujiko@gmail.com>
2024-03-16 14:55:24 +0000

    Initial files

---
+++ Arrayaverage.java
@@ -0,0 +1,8 @@
+//Create an application that asks a user to enter how many subjects they have. The application should then ask the user to enter the results for each subject. Store these results in an array and print them back to the user along with their overall average and grade for each module.
+//Neil McCarthy - x23319828
+
+public class Arrayaverage {
+    public static void main(String[] args){
+        
+    }
+}
